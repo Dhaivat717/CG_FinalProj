eb314a5adf1f37508c49773f01172e10046a5eda
Chaitanya Vallabhaneni <45491114+cv261@users.noreply.github.com>
2021-12-19 18:34:49 -0500

    Fixed shape

--- Assets/Scripts/DebugShapes.cs
+++ Assets/Scripts/DebugShapes.cs
@@ -1,33 +1,22 @@
-﻿/* 
- * This file is part of Unity-Procedural-IK-Wall-Walking-Spider on github.com/PhilS94
- * It is an extension of DebugX by Hayden Scott-Baron (Dock)
- * Copyright (C) 2020 Philipp Schofield - All Rights Reserved
- */
-
-
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
-/*
- * Class for debug drawing certain shapes.
- * Creates the specified shapes by the use of multiple Debug.DrawLine calls.
- */
 
 public class DebugShapes : MonoBehaviour {
 
-    public static void DrawLine(Vector3 pos, Vector3 end, Color col, float duration = 0f) {
-        Debug.DrawLine(pos, end, col, duration);
+    public static void DrawLine(Vector3 pos, Vector3 end, Color col, float dur = 0f) {
+        Debug.DrawLine(pos, end, col, dur);
     }
 
-    public static void DrawRay(Vector3 pos, Vector3 direction, Color col, float duration = 0f) {
-        Debug.DrawLine(pos, pos + direction, col, duration);
+    public static void DrawRay(Vector3 pos, Vector3 direction, Color col, float dur = 0f) {
+        Debug.DrawLine(pos, pos + direction, col, dur);
     }
 
-    public static void DrawRay(Vector3 pos, Vector3 direction, float distance, Color col, float duration = 0f) {
-        Debug.DrawLine(pos, pos + direction.normalized * distance, col, duration);
+    public static void DrawRay(Vector3 pos, Vector3 direction, float distance, Color col, float dur = 0f) {
+        Debug.DrawLine(pos, pos + direction.normalized * distance, col, dur);
     }
 
-    public static void DrawPoint(Vector3 pos, Color col, float scale, float duration = 0.0f) {
+    public static void DrawPoint(Vector3 pos, Color col, float scale, float dur = 0.0f) {
         Vector3[] points = new Vector3[]
         {
             pos + (Vector3.up * scale),
@@ -38,28 +27,28 @@ public class DebugShapes : MonoBehaviour {
             pos - (Vector3.forward * scale)
         };
 
-        Debug.DrawLine(points[0], points[1], col, duration);
-        Debug.DrawLine(points[2], points[3], col, duration);
-        Debug.DrawLine(points[4], points[5], col, duration);
+        Debug.DrawLine(points[0], points[1], col, dur);
+        Debug.DrawLine(points[2], points[3], col, dur);
+        Debug.DrawLine(points[4], points[5], col, dur);
 
-        Debug.DrawLine(points[0], points[2], col, duration);
-        Debug.DrawLine(points[0], points[3], col, duration);
-        Debug.DrawLine(points[0], points[4], col, duration);
-        Debug.DrawLine(points[0], points[5], col, duration);
+        Debug.DrawLine(points[0], points[2], col, dur);
+        Debug.DrawLine(points[0], points[3], col, dur);
+        Debug.DrawLine(points[0], points[4], col, dur);
+        Debug.DrawLine(points[0], points[5], col, dur);
 
-        Debug.DrawLine(points[1], points[2], col, duration);
-        Debug.DrawLine(points[1], points[3], col, duration);
-        Debug.DrawLine(points[1], points[4], col, duration);
-        Debug.DrawLine(points[1], points[5], col, duration);
+        Debug.DrawLine(points[1], points[2], col, dur);
+        Debug.DrawLine(points[1], points[3], col, dur);
+        Debug.DrawLine(points[1], points[4], col, dur);
+        Debug.DrawLine(points[1], points[5], col, dur);
 
-        Debug.DrawLine(points[4], points[2], col, duration);
-        Debug.DrawLine(points[4], points[3], col, duration);
-        Debug.DrawLine(points[5], points[2], col, duration);
-        Debug.DrawLine(points[5], points[3], col, duration);
+        Debug.DrawLine(points[4], points[2], col, dur);
+        Debug.DrawLine(points[4], points[3], col, dur);
+        Debug.DrawLine(points[5], points[2], col, dur);
+        Debug.DrawLine(points[5], points[3], col, dur);
 
     }
 
-    public static void DrawCube(Vector3 pos, Color col, Vector3 scale, float duration = 0f) {
+    public static void DrawCube(Vector3 pos, Color col, Vector3 scale, float dur = 0f) {
         Vector3 t = scale * 0.5f;
 
         Vector3[] p = new Vector3[]
@@ -74,10 +63,10 @@ public class DebugShapes : MonoBehaviour {
             pos + new Vector3(t.x,      -t.y,   -t.z),
         };
 
-        Debug.DrawLine(p[0], p[1], col, duration);
-        Debug.DrawLine(p[1], p[2], col, duration);
-        Debug.DrawLine(p[2], p[3], col, duration);
-        Debug.DrawLine(p[3], p[0], col, duration);
+        Debug.DrawLine(p[0], p[1], col, dur);
+        Debug.DrawLine(p[1], p[2], col, dur);
+        Debug.DrawLine(p[2], p[3], col, dur);
+        Debug.DrawLine(p[3], p[0], col, dur);
     }
 
     public static void DrawRect(Rect rect, Color col) {
@@ -104,7 +93,7 @@ public class DebugShapes : MonoBehaviour {
         Debug.DrawLine(p[3], p[0], col);
     }
 
-    public static void DrawPlane(Vector3 pos, Vector3 normal, Vector3 tangent, float size, Color col, float duration = 0) {
+    public static void DrawPlane(Vector3 pos, Vector3 normal, Vector3 tangent, float size, Color col, float dur = 0) {
 
         tangent = tangent.normalized;
         normal = normal.normalized;
@@ -115,19 +104,19 @@ public class DebugShapes : MonoBehaviour {
         Vector3 c = pos - 0.5f * size * tangent - 0.5f * size * cross;
         Vector3 d = pos + 0.5f * size * tangent - 0.5f * size * cross;
 
-        Debug.DrawLine(a, b, col, duration);
-        Debug.DrawLine(a, c, col, duration);
-        Debug.DrawLine(a, d, col, duration);
-        Debug.DrawLine(b, c, col, duration);
-        Debug.DrawLine(b, d, col, duration);
-        Debug.DrawLine(c, d, col, duration);
+        Debug.DrawLine(a, b, col, dur);
+        Debug.DrawLine(a, c, col, dur);
+        Debug.DrawLine(a, d, col, dur);
+        Debug.DrawLine(b, c, col, dur);
+        Debug.DrawLine(b, d, col, dur);
+        Debug.DrawLine(c, d, col, dur);
     }
 
-    public static void DrawCircle(Vector3 pos, Vector3 normal, float radius, Color col, float duration = 0) {
+    public static void DrawCircle(Vector3 pos, Vector3 normal, float radius, Color col, float dur = 0) {
 
         int l = 16;
 
-        //Choose a perpendicular vector
+    
         Vector3 perpendicular;
         perpendicular = Vector3.ProjectOnPlane(Vector3.forward, normal);
         if (perpendicular == Vector3.zero) {
@@ -138,19 +127,19 @@ public class DebugShapes : MonoBehaviour {
 
         Vector3[] p = new Vector3[l];
 
-        //Lerping is problematic with an angle greater than 180
+     
         for (int k = 0; k < l; k++) {
             p[k] = pos + Quaternion.AngleAxis(360.0f * k / l, normal) * perpendicular * radius;
         }
 
-        // Draw circle
+  
         for (int k = 0; k < l - 1; k++) {
-            Debug.DrawLine(p[k], p[k + 1], col, duration);
+            Debug.DrawLine(p[k], p[k + 1], col, dur);
         }
-        Debug.DrawLine(p[l - 1], p[0], col, duration);
+        Debug.DrawLine(p[l - 1], p[0], col, dur);
     }
 
-    public static void DrawCircleSection(Vector3 pos, Vector3 min, Vector3 max, Vector3 normal, float minRadius, float maxRadius, Color col, float duration = 0) {
+    public static void DrawCircleSection(Vector3 pos, Vector3 min, Vector3 max, Vector3 normal, float minRadius, float maxRadius, Color col, float dur = 0) {
 
         if (min == Vector3.zero || max == Vector3.zero) {
             Debug.LogWarning("Min and Max Vector not allowed to be zero.");
@@ -180,18 +169,17 @@ public class DebugShapes : MonoBehaviour {
             P[k] = pos + Quaternion.AngleAxis(angle * k / (l - 1), normal) * projMin * maxRadius;
         }
 
-        // Draw circles
+        
         for (int k = 0; k < l - 1; k++) {
-            Debug.DrawLine(p[k], p[k + 1], col, duration);
-            Debug.DrawLine(P[k], P[k + 1], col, duration);
+            Debug.DrawLine(p[k], p[k + 1], col, dur);
+            Debug.DrawLine(P[k], P[k + 1], col, dur);
         }
 
-        // Connect inner to outer circle 
-        Debug.DrawLine(p[0], P[0], col, duration);
-        Debug.DrawLine(p[l - 1], P[l - 1], col, duration);
+        Debug.DrawLine(p[0], P[0], col, dur);
+        Debug.DrawLine(p[l - 1], P[l - 1], col, dur);
     }
 
-    public static void DrawSphere(Vector3 pos, float radius, Color col, float duration = 0) {
+    public static void DrawSphere(Vector3 pos, float radius, Color col, float dur = 0) {
 
         int l = 3;
 
@@ -206,21 +194,19 @@ public class DebugShapes : MonoBehaviour {
             vert[k] = pos - Vector3.up * radius + (k + 1) * step * Vector3.up;
             Z[k] = pos - Vector3.forward * radius + (k + 1) * step * Vector3.forward;
             float angle = Mathf.Lerp(-Mathf.PI / 2, Mathf.PI / 2, (float)(k + 1) / (l + 1));
-            float r = Mathf.Cos(angle) * radius; //Calculated wrong
-            DrawCircle(horiz[k], Vector3.right, r, col, duration);
-            DrawCircle(vert[k], Vector3.up, r, col, duration);
-            DrawCircle(Z[k], Vector3.forward, r, col, duration);
+            float r = Mathf.Cos(angle) * radius; 
+            DrawCircle(horiz[k], Vector3.right, r, col, dur);
+            DrawCircle(vert[k], Vector3.up, r, col, dur);
+            DrawCircle(Z[k], Vector3.forward, r, col, dur);
         }
-        //Camera cam = UnityEditor.SceneView.lastActiveSceneView.camera;
-        //if (cam != null) DrawCircle(pos, -cam.transform.forward, radius, col);
+        
     }
 
     public static void DrawSphereSection(Vector3 pos, Vector3 lowLeft, Vector3 lowRight, Vector3 upLeft, Vector3 upRight, float minRadius, float maxRadius, Color col) {
 
         int l = 9;
 
-        // Make surecorners make sense
-        // Make sure every vector is normalized
+        
 
         Vector3[] v = new Vector3[l];
         Vector3[] w = new Vector3[l];
@@ -285,36 +271,28 @@ public class DebugShapes : MonoBehaviour {
             q_down[k] = q[k] - normal * lowHeight;
         }
 
-        //Connect the p's
+    
         for (int k = 0; k < l - 1; k++) {
             Debug.DrawLine(p[k], p[k + 1], col);
             Debug.DrawLine(p_up[k], p_up[k + 1], col);
             Debug.DrawLine(p_down[k], p_down[k + 1], col);
         }
-        /*
-        for (int k = 0; k < l; k++) {
-            Debug.DrawLine(p_up[k], p_down[k], col);
-        }
-        */
+        
         Debug.DrawLine(p_up[0], p_down[0], col);
         Debug.DrawLine(p_up[l - 1], p_down[l - 1], col);
 
-        //Connect the q's
+
         for (int k = 0; k < l - 1; k++) {
             Debug.DrawLine(q[k], q[k + 1], col);
             Debug.DrawLine(q_up[k], q_up[k + 1], col);
             Debug.DrawLine(q_down[k], q_down[k + 1], col);
         }
-        /*
-        for (int k = 0; k < l; k++) {
-            Debug.DrawLine(q_up[k], q_down[k], col);
-        }
-        */
+       
         Debug.DrawLine(q_up[0], q_down[0], col);
         Debug.DrawLine(q_up[l - 1], q_down[l - 1], col);
 
 
-        //Connect the p's with q's  Sides
+    
         Debug.DrawLine(p[0], q[0], col);
         Debug.DrawLine(p[l - 1], q[l - 1], col);
 
@@ -324,28 +302,22 @@ public class DebugShapes : MonoBehaviour {
         Debug.DrawLine(p_down[0], q_down[0], col);
         Debug.DrawLine(p_down[l - 1], q_down[l - 1], col);
 
-        /*
-        //Connect the p's with q's  Top and Bottom
-        for (int k = 0; k < l; k++) {
-            Debug.DrawLine(p_up[k], q_up[k], col);
-            Debug.DrawLine(p_down[k], q_down[k], col);
-        }
-        */
+
     }
 
-    public static void DrawSphereRay(Vector3 start, Vector3 direction, float distance, float radius, int amount, Color col, float duration = 0) {
+    public static void DrawSphereRay(Vector3 start, Vector3 direction, float distance, float radius, int amount, Color col, float dur = 0) {
 
         Vector3 endPoint = start + (radius + distance) * direction;
         Vector3 endPointSphereCenter = endPoint - (radius * direction);
 
         for (int i = 0; i < amount; i++) {
-            DrawSphere(Vector3.Lerp(start, endPointSphereCenter, (float)i / (amount - 1)), radius, new Color(col.r, col.g, col.b, 0.5f), duration);
+            DrawSphere(Vector3.Lerp(start, endPointSphereCenter, (float)i / (amount - 1)), radius, new Color(col.r, col.g, col.b, 0.5f), dur);
         }
-        Debug.DrawLine(start, endPoint, col, duration);
+        Debug.DrawLine(start, endPoint, col, dur);
     }
 
-    public static void DrawSphereRay(Vector3 start, Vector3 end, float radius, int amount, Color col, float duration = 0) {
+    public static void DrawSphereRay(Vector3 start, Vector3 end, float radius, int amount, Color col, float dur = 0) {
         Vector3 v = end - start;
-        DrawSphereRay(start, v.normalized, v.magnitude, radius, amount, col, duration);
+        DrawSphereRay(start, v.normalized, v.magnitude, radius, amount, col, dur);
     }
 }
