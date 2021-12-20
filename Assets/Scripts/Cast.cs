f0f8975ee5f259d89586e56adddab510c030dedd
Dhaivat Patel <79534543+Dhaivat717@users.noreply.github.com>
2021-12-19 19:18:39 -0500

    Update

--- Assets/Scripts/Cast.cs
+++ Assets/Scripts/Cast.cs
@@ -1,181 +1,253 @@
-﻿/* 
- * This file is part of Unity-Procedural-IK-Wall-Walking-Spider on github.com/PhilS94
- * Copyright (C) 2020 Philipp Schofield - All Rights Reserved
- */
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Raycasting {
+namespace Raycasting
+{
 
-    public enum CastMode {
+    public enum CastMode
+    {
         RayCast,
         SphereCast
     }
+    public class PositionRelative
+    {
+        private Vector3 pos;
+        private Transform left;
 
-    /*
-     * Class representing a position relative to a given parent transform.
-     * Basically mimics parenting an object to a parent, but only for position, without creating new transforms.
-     */
-public class PositionRelative {
-        private Vector3 position;
-        private Transform parent;
 
-        public PositionRelative(Vector3 m_position) {
-            position = m_position;
-            parent = null;
+        public void setParent(Transform enPos)
+        {
+            Vector3 old_pos = getWorldPosition();
+            left = enPos;
+            setWorldPosition(old_pos);
+        }
+        public PositionRelative(Vector3 newP, Transform enPos)
+        {
+            left = enPos;
+            setWorldPosition(newP);
+        }
+        public PositionRelative(Vector3 newP)
+        {
+            pos = newP;
+            left = null;
         }
 
-        public PositionRelative(Vector3 m_position, Transform m_parent) {
-            parent = m_parent;
-            setWorldPosition(m_position);
+        public Vector3 getLocalPosition()
+        {
+            return pos;
+        }
+        public void setWorldPosition(Vector3 newP)
+        {
+            if (left == null)
+            {
+                pos = newP;
+            }
+            else
+            {
+                pos = left.InverseTransformPoint(newP);
+            }
         }
 
-        public Vector3 getWorldPosition() { return (parent == null) ? position : parent.TransformPoint(position); }
-        public Vector3 getLocalPosition() { return position; }
-        public Transform getParent() { return parent; }
-        public void setWorldPosition(Vector3 m_position) { position = (parent == null) ? m_position : parent.InverseTransformPoint(m_position); }
-        public void setLocalPosition(Vector3 m_position) { position = m_position; }
-        public void setParent(Transform m_parent) {
-            Vector3 old_Position = getWorldPosition();
-            parent = m_parent;
-            setWorldPosition(old_Position);
+        public Vector3 getWorldPosition()
+        {
+            if (left == null)
+            {
+                return pos;
+            }
+            else
+            {
+                return left.TransformPoint(pos);
+            }
         }
+        public void setLocalPosition(Vector3 newP)
+        {
+            pos = newP;
+        }
+
+
+        public Transform getParent()
+        {
+            return left;
+        }
+
     }
+    public abstract class Cast
+    {
 
-    /*
-     * Abstract class representing a raycast by origin and end point.
-     * Provides us with functions for casting as well as debug drawing.
-     * The power of this class is that the origin and end points are relative positions, which enables us to parent them to
-     * other transforms. Thus, if a raycasts origin and end point are always fixed relative to a certain transform,
-     * the raycast would only need to be initialized once and never in need for an update.
-     * 
-     * The abstractness follows from the fact that a cast can either be a raycast or a spherecast,
-     * each with their own casting and debug drawing functions. Both classes are implemented below.
-     */
-    public abstract class Cast {
+        protected PositionRelative start;
+        protected PositionRelative dest;
 
-        protected PositionRelative origin;
-        protected PositionRelative end;
-
-        public Cast() {
-            origin = new PositionRelative(Vector3.zero);
-            end = new PositionRelative(Vector3.zero);
+        public Cast()
+        {
+            start = new PositionRelative(Vector3.zero);
+            dest = new PositionRelative(Vector3.zero);
         }
 
-        public Cast(Vector3 m_origin, Vector3 m_direction, float m_distance) {
-            origin = new PositionRelative(m_origin);
-            end = new PositionRelative(m_origin + m_direction.normalized * m_distance);
+        public Cast(Vector3 newSt, Vector3 angle, float pathLen)
+        {
+            start = new PositionRelative(newSt);
+            dest = new PositionRelative(newSt + angle.normalized * pathLen);
         }
 
-        public Cast(Vector3 m_origin, Vector3 m_end) {
-            origin = new PositionRelative(m_origin);
-            end = new PositionRelative(m_end);
+        public Cast(Vector3 newSt, Vector3 enNew)
+        {
+            start = new PositionRelative(newSt);
+            dest = new PositionRelative(enNew);
         }
 
-        public Cast(Vector3 m_origin, Vector3 m_direction, float m_distance, Transform m_parentOrigin, Transform m_parentEnd) {
-            origin = new PositionRelative(m_origin, m_parentOrigin);
-            end = new PositionRelative(m_origin + m_direction.normalized * m_distance, m_parentEnd);
+        public void setOrigin(Vector3 newSt)
+        {
+            start.setWorldPosition(newSt);
         }
 
-        public Cast(Vector3 m_origin, Vector3 m_end, Transform m_parentOrigin, Transform m_parentEnd) {
-            origin = new PositionRelative(m_origin, m_parentOrigin);
-            end = new PositionRelative(m_end, m_parentEnd);
+        public Cast(Vector3 newSt, Vector3 angle, float pathLen, Transform oriSt, Transform destEn)
+        {
+            start = new PositionRelative(newSt, oriSt);
+            dest = new PositionRelative(newSt + angle.normalized * pathLen, destEn);
         }
-
-        // Returns values in World Space
-        public Vector3 getOrigin() { return origin.getWorldPosition(); }
-        public Vector3 getDirection() { return (end.getWorldPosition() - origin.getWorldPosition()); }
-        public float getDistance() { return getDirection().magnitude; }
-        public Vector3 getEnd() { return end.getWorldPosition(); }
-
-        public void setParentOrigin(Transform m_parent) { origin.setParent(m_parent); }
-        public void setParentEnd(Transform m_parent) { end.setParent(m_parent); }
-        public void setOrigin(Vector3 m_Origin) { origin.setWorldPosition(m_Origin); }
-        public void setEnd(Vector3 m_end) { end.setWorldPosition(m_end); }
-        public void setDistance(float m_distance) { Vector3 p = getOrigin(); end.setWorldPosition(p + (getEnd() - p).normalized * m_distance); }
         public abstract bool castRay(out RaycastHit hitInfo, LayerMask layerMask, QueryTriggerInteraction q = QueryTriggerInteraction.Ignore);
+        public Vector3 getOrigin()
+        {
+            return start.getWorldPosition();
+        }
+
+        public abstract void draw(Color col, float duration = 0);
+        public Vector3 getDirection()
+        {
+            return (dest.getWorldPosition() - start.getWorldPosition());
+        }
+        public float getDistance()
+        {
+            return getDirection().magnitude;
+        }
 
         public abstract RaycastHit[] castRayAll(LayerMask layerMask, QueryTriggerInteraction q = QueryTriggerInteraction.Ignore);
+        public void setParentOrigin(Transform enPos)
+        {
+            start.setParent(enPos);
+        }
+        public void setParentEnd(Transform enPos)
+        {
+            dest.setParent(enPos);
+        }
+        public void setEnd(Vector3 enNew)
+        {
+            dest.setWorldPosition(enNew);
+        }
 
-        public abstract void draw(Color col, float duration=0);
+        public Cast(Vector3 newSt, Vector3 enNew, Transform oriSt, Transform destEn)
+        {
+            start = new PositionRelative(newSt, oriSt);
+            dest = new PositionRelative(enNew, destEn);
+        }
+        public Vector3 getEnd()
+        {
+            return dest.getWorldPosition();
+        }
+        public void setDistance(float pathLen)
+        {
+            Vector3 p = getOrigin(); dest.setWorldPosition(p + (getEnd() - p).normalized * pathLen);
+        }
+
     }
 
 
-    public class RayCast : Cast {
+    public class RayCast : Cast
+    {
 
         public RayCast() : base() { }
 
-        public RayCast(Vector3 m_origin, Vector3 m_direction, float m_distance) : base(m_origin, m_direction, m_distance) { }
-
-        public RayCast(Vector3 m_origin, Vector3 m_end) : base(m_origin, m_end) { }
-
-        public RayCast(Vector3 m_origin, Vector3 m_direction, float m_distance, Transform m_parentOrigin, Transform m_parentEnd) : base(m_origin, m_direction, m_distance, m_parentOrigin, m_parentEnd) { }
-
-        public RayCast(Vector3 m_origin, Vector3 m_end, Transform m_parentOrigin, Transform m_parentEnd) : base(m_origin, m_end, m_parentOrigin, m_parentEnd) { }
-
-
-        public override bool castRay(out RaycastHit hitInfo, LayerMask layerMask, QueryTriggerInteraction q = QueryTriggerInteraction.Ignore) {
+        public RayCast(Vector3 newSt, Vector3 angle, float pathLen) : base(newSt, angle, pathLen) { }
+        public override bool castRay(out RaycastHit hitInfo, LayerMask layerMask, QueryTriggerInteraction q = QueryTriggerInteraction.Ignore)
+        {
             Vector3 v = getDirection();
             return Physics.Raycast(getOrigin(), v.normalized, out hitInfo, v.magnitude, layerMask, q);
         }
 
-        public override RaycastHit[] castRayAll(LayerMask layerMask, QueryTriggerInteraction q = QueryTriggerInteraction.Ignore) {
+        public RayCast(Vector3 newSt, Vector3 enNew) : base(newSt, enNew) { }
+
+        public override RaycastHit[] castRayAll(LayerMask layerMask, QueryTriggerInteraction q = QueryTriggerInteraction.Ignore)
+        {
             Vector3 v = getDirection();
             return Physics.RaycastAll(getOrigin(), v, v.magnitude, layerMask, q);
         }
 
-        public override void draw(Color col, float duration=0) {
-            Debug.DrawLine(getOrigin(), getEnd(), col,duration);
+        public RayCast(Vector3 newSt, Vector3 angle, float pathLen, Transform oriSt, Transform destEn) : base(newSt, angle, pathLen, oriSt, destEn) { }
+
+        public RayCast(Vector3 newSt, Vector3 enNew, Transform oriSt, Transform destEn) : base(newSt, enNew, oriSt, destEn) { }
+
+        public override void draw(Color col, float duration = 0)
+        {
+            Debug.DrawLine(getOrigin(), getEnd(), col, duration);
         }
     }
 
 
-    public class SphereCast : Cast {
-        protected float radius;
+    public class SphereCast : Cast
+    {
+        protected float distFOri;
 
-        public SphereCast() : base() {
-            radius = 1.0f;
+        public SphereCast() : base()
+        {
+            distFOri = 1.0f;
         }
 
-        public SphereCast(float m_radius) : base() {
-            radius = m_radius;
+
+
+        public SphereCast(Vector3 newSt, Vector3 angle, float pathLen, float deltaNew) : base(newSt, angle, pathLen)
+        {
+            distFOri = deltaNew;
         }
-
-        public SphereCast(Vector3 m_origin, Vector3 m_direction, float m_distance, float m_radius) : base(m_origin, m_direction, m_distance) {
-            radius = m_radius;
-        }
-
-        public SphereCast(Vector3 m_origin, Vector3 m_end, float m_radius) : base(m_origin, m_end) {
-            radius = m_radius;
-        }
-
-        public SphereCast(Vector3 m_origin, Vector3 m_direction, float m_distance, float m_radius,Transform m_parentOrigin, Transform m_parentEnd) : base(m_origin, m_direction, m_distance, m_parentOrigin, m_parentEnd) {
-            setRadius(m_radius);
-        }
-
-        public SphereCast(Vector3 m_origin, Vector3 m_end, float m_radius, Transform m_parentOrigin,Transform m_parentEnd) : base(m_origin, m_end, m_parentOrigin, m_parentEnd) {
-            setRadius(m_radius);
-        }
-
-        // Transform radius with origin Transforms lossyscale.z
-        public float getRadius() { return (origin.getParent() == null) ? radius : origin.getParent().lossyScale.z * radius; }
-
-        public void setRadius(float m_radius) { radius = (origin.getParent() == null) ? m_radius : m_radius / origin.getParent().lossyScale.z; }
-
-        public override bool castRay(out RaycastHit hitInfo, LayerMask layerMask, QueryTriggerInteraction q = QueryTriggerInteraction.Ignore) {
+        public override bool castRay(out RaycastHit hitInfo, LayerMask layerMask, QueryTriggerInteraction q = QueryTriggerInteraction.Ignore)
+        {
             Vector3 v = getDirection();
             return Physics.SphereCast(getOrigin(), getRadius(), v.normalized, out hitInfo, v.magnitude, layerMask, q);
         }
 
-        public override RaycastHit[] castRayAll(LayerMask layerMask, QueryTriggerInteraction q = QueryTriggerInteraction.Ignore) {
+
+        public SphereCast(Vector3 newSt, Vector3 angle, float pathLen, float deltaNew, Transform oriSt, Transform destEn) : base(newSt, angle, pathLen, oriSt, destEn)
+        {
+            setRadius(deltaNew);
+        }
+
+        public SphereCast(Vector3 newSt, Vector3 enNew, float deltaNew, Transform oriSt, Transform destEn) : base(newSt, enNew, oriSt, destEn)
+        {
+            setRadius(deltaNew);
+        }
+
+        public float getRadius()
+        {
+
+            if (start.getParent() == null)
+            {
+                return distFOri;
+            }
+            else
+            {
+                return start.getParent().lossyScale.z * distFOri;
+            }
+        }
+        public SphereCast(float deltaNew) : base()
+        {
+            distFOri = deltaNew;
+        }
+
+        public void setRadius(float deltaNew) { distFOri = (start.getParent() == null) ? deltaNew : deltaNew / start.getParent().lossyScale.z; }
+        public override RaycastHit[] castRayAll(LayerMask layerMask, QueryTriggerInteraction q = QueryTriggerInteraction.Ignore)
+        {
             Vector3 v = getDirection();
             return Physics.SphereCastAll(getOrigin(), getRadius(), v.normalized, v.magnitude, layerMask, q);
         }
 
-        public override void draw(Color col,float duration=0) {
-            DebugShapes.DrawSphereRay(getOrigin(), getEnd(), getRadius(), 5, col,duration);
+
+        public override void draw(Color col, float duration = 0)
+        {
+            DebugShapes.DrawSphereRay(getOrigin(), getEnd(), getRadius(), 5, col, duration);
+        }
+        public SphereCast(Vector3 newSt, Vector3 enNew, float deltaNew) : base(newSt, enNew)
+        {
+            distFOri = deltaNew;
         }
     }
 }
